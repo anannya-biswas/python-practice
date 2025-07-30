167a82d059dcc7b2950b1906371590d2b68a550c
anannya-biswas <133083620+anannya-biswas@users.noreply.github.com>
2025-07-30 22:09:41 +0530

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,9 @@
+import random
+
+while True:
+    roll = input("Roll the dice? (y/n): ")
+    if roll.lower() == 'y':
+        print("You rolled:", random.randint(1, 6))
+    else:
+        print("Goodbye!")
+        break
