982f1505e06a6e0b72feba8adb1011b9f6a12de2
anannya-biswas <133083620+anannya-biswas@users.noreply.github.com>
2025-07-31 19:21:31 +0530

    rock-paper-scissors.py

---
+++ rock-paper-scissors.py
@@ -0,0 +1,24 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+while True:
+    user = input("Rock, paper or scissors? (or 'exit' to quit): ").lower()
+    if user == 'exit':
+        break
+
+    if user not in choices:
+        print("Invalid input.")
+        continue
+
+    computer = random.choice(choices)
+    print(f"Computer chose {computer}")
+
+    if user == computer:
+        print("It's a tie!")
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "paper" and computer == "rock") or \
+         (user == "scissors" and computer == "paper"):
+        print("You win!")
+    else:
+        print("Computer wins!")
